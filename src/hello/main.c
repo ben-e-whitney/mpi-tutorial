e0addba4eff3c41e282f35e4b5a7145760ea3760
Ben Whitney <ben_whitney@brown.edu>
2016-08-19 17:39:43 -0400

    Switch to C-style comments.

--- src/hello/main.c
+++ src/hello/main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-//Include the MPI header.
+/*Include the MPI header.*/
 
 int main(int argc, char **argv) {
-    //Initialize MPI.
+    /*Initialize MPI.*/
     int rank;
     int size;
-    //Get this process' rank.
-    //Get the number of processes.
+    /*Get this process' rank.*/
+    /*Get the number of processes.*/
     printf("Hello world! I am rank %d of %d.\n", rank, size);
-    //Finalize MPI.
+    /*Finalize MPI.*/
     return 0;
 }
